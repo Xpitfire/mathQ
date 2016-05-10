d0982e0739a6bfede50d1ac59faa9f032646e448
Marius-Constantin Dinu <dinu.marius-constantin@hotmail.com>
2016-05-09 23:59:39 -0400

    Added project files for UI development with Xamarin.

---
+++ src/mathQ.Prototype.App/mathQ.Prototype.App/Pages/ClassifierPage.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+
+namespace mathQ.Prototype.App.Pages
+{
+    public partial class ClassifierPage : ContentPage
+    {
+        public ClassifierPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/mathQ.Prototype.App/mathQ.Prototype.App/Views/SolutionView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace mathQ.Prototype.App.Views
+{
+    public class SolutionView : ContentView
+    {
+        public SolutionView()
+        {
+            Content = new Grid
+            {
+                ColumnDefinitions = new ColumnDefinitionCollection
+                {
+                    new ColumnDefinition { Width = new GridLength(2, GridUnitType.Star) },
+                    new ColumnDefinition { Width = new GridLength(1, GridUnitType.Star) }
+                },
+                Children =
+                {
+                    new Label(),
+                    new Button()
+                }
+            };
+        }
+    }
+}
